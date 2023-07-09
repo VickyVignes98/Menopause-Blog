26b5e1f25a808987c1a059aae57142897664224e
Vignes <vickymario73@gmail.com>
2023-07-09 19:55:18 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+       plugins: [require("daisyui")],
+
+        // daisyUI config (optional)
+ daisyui: {
+    styled: true,
+    themes: false,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+
+  }
